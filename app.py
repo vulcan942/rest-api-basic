bb7e6b3350da63ae6aea3292499ea16f31b847f4
Nishant Choudhary <ncnishant9868@gmail.com>
2021-07-20 22:26:57 +0530

    added postgres db changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-
+import os
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
@@ -11,7 +11,9 @@ from db import db
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "sqlite:///data.db"
+)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.debug = True
 app.config["SECRET_KEY"] = "nishant"
